19a22e496861901432849bd6e36bd1479739dd72
feilong <venusdrogon@163.com>
2014-05-05 16:34:20 +0700

    add default feilong-pager.properties

--- taglib/feilong-taglib-else/src/main/java/com/feilong/taglib/display/table/package-info.java
+++ taglib/feilong-taglib-else/src/main/java/com/feilong/taglib/display/table/package-info.java
@@ -13,15 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/**
- * 分页工具包,支持自定义标签(用于 jsp view),支持纯VM解析(用于Velocity view)
- * 
- * @author <a href="mailto:venusdrogon@163.com">金鑫</a>
- * 
- * @since 1.0.0
- * 
- * @version 1.0.0 2010-2-3 下午01:03:14
- * @version 1.0.5 May 3, 2014 1:44:08 PM
- */
-package com.feilong.taglib.display.pager;
+@Deprecated
+package com.feilong.taglib.display.table;
