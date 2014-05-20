1bdb1aa666b7a42b65f0992e2b8798376dfa998b
feilong <venusdrogon@163.com>
2014-05-20 20:25:24 +0700

    move sql package to feilong-db-jdbc project

--- commons/feilong-db-jdbc/src/main/java/com/feilong/commons/db/ResultSetUtil.java
+++ commons/feilong-db-jdbc/src/main/java/com/feilong/commons/db/ResultSetUtil.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.feilong.commons.core.sql;
+package com.feilong.commons.db;
 
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
