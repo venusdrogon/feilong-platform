610da15b29c17516e29c91df46e2147152275001
feilong <venusdrogon@163.com>
2014-05-22 16:11:25 +0700

    重写了 Validator 内部方法,支持所有的数组判断

--- commons/feilong-core/src/main/java/com/feilong/commons/core/util/Validator.java
+++ commons/feilong-core/src/main/java/com/feilong/commons/core/util/Validator.java
@@ -20,34 +20,32 @@ import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * <p>
- * 通用验证类,feilong-core核心类之一,判断对象是否是null或者空
+ * 判断对象是否为null或者Empty,feilong-core核心类之一.
  * </p>
  * <ol>
  * <li>{@link #isNullOrEmpty(Object)} 判断对象是否是null或者空</li>
  * <li>{@link #isNotNullOrEmpty(Object)}判断对象是否不是null或者不是空</li>
  * </ol>
- * .
  * 
  * @author <a href="mailto:venusdrogon@163.com">金鑫</a>
  * @version 1.0 Sep 2, 2010 8:35:28 PM
- * @version 1.0.1 2012-9-23 21:34 api更新,rename method,isNullOrEmpty替换isNull
- * @since 1.0.0
+ * @version 1.0.1 2012-9-23 21:34 rename method,isNullOrEmpty替代isNull
+ * @version 1.0.7 2014-5-22 15:57 add {@link #arrayIsNullOrEmpty(Object)}
  * @see String#trim()
  * @see Map#isEmpty()
  * @see Collection#isEmpty()
  * @see Enumeration#hasMoreElements()
  * @see Iterator#hasNext()
+ * @since 1.0.0
  */
 public final class Validator{
-
-	/** The Constant log. */
-	@SuppressWarnings("unused")
-	private final static Logger	log	= LoggerFactory.getLogger(Validator.class);
 
 	/** Don't let anyone instantiate this class. */
 	private Validator(){}
@@ -60,50 +58,78 @@ public final class Validator{
 	 * <ol>
 	 * <li>{@link Collection},使用其 {@link Collection#isEmpty()}</li>
 	 * <li>{@link Map},使用其 {@link Map#isEmpty()}</li>
-	 * <li><code>Object[]</code>,判断length==0</li>
 	 * <li>{@link String},使用 {@link String#trim()}.length()<=0效率高</li>
 	 * <li>{@link Enumeration},使用 {@link Enumeration#hasMoreElements()}</li>
 	 * <li>{@link Iterator},使用 {@link Iterator#hasNext()}</li>
+	 * <li><code>Object[]</code>,判断length==0,注二维数组不管是primitive 还是包装类型,都instanceof Object[]</li>
+	 * <li><code>byte[]</code>,判断length==0</li>
+	 * <li><code>boolean[]</code>,判断length==0</li>
+	 * <li><code>char[]</code>,判断length==0</li>
+	 * <li><code>int[]</code>,判断length==0</li>
+	 * <li><code>short[]</code>,判断length==0</li>
+	 * <li><code>float[]</code>,判断length==0</li>
+	 * <li><code>double[]</code>,判断length==0</li>
 	 * </ol>
 	 * 
 	 * @param value
-	 *            可以是Collection,Map,Object[],String,Enumeration,Iterator类型
-	 * @return 如果是null,返回true;如果是空也返回true,其他情况返回false
+	 *            可以是Collection,Map,String,Enumeration,Iterator,以及所有数组类型
+	 * @return 如果是null,返回true<br>
+	 *         如果是empty也返回true<br>
+	 *         其他情况返回false<br>
+	 *         如果不是上述类型,不判断empty,返回false
+	 * @see CollectionUtils#isEmpty(Collection)
+	 * @see CollectionUtils#isNotEmpty(Collection)
+	 * @see StringUtils#isBlank(String)
+	 * @see StringUtils#isEmpty(String)
+	 * @see MapUtils#isEmpty(Map)
+	 * @see MapUtils#isNotEmpty(Map)
+	 * @see ArrayUtils#isEmpty(byte[])
+	 * @see ArrayUtils#isEmpty(boolean[])
+	 * @see ArrayUtils#isEmpty(char[])
+	 * @see ArrayUtils#isEmpty(int[])
+	 * @see ArrayUtils#isEmpty(long[])
+	 * @see ArrayUtils#isEmpty(short[])
+	 * @see ArrayUtils#isEmpty(float[])
+	 * @see ArrayUtils#isEmpty(double[])
+	 * @see ArrayUtils#isEmpty(Object[])
 	 */
-	@SuppressWarnings("rawtypes")
 	public final static boolean isNullOrEmpty(Object value){
 		if (null == value){
 			return true;
 		}
 		// *****************************************************************************
-		boolean flag = false;
+
 		// 字符串
-		if (value instanceof String){
-			// 比较字符串长度, 效率高
-			flag = value.toString().trim().length() <= 0;
+		if (value instanceof String){// 比较字符串长度, 效率高
+			return value.toString().trim().length() <= 0;
 		}
-		// Object[]object数组
-		else if (value instanceof Object[]){
-			flag = ((Object[]) value).length == 0;
-		}
-		// ***********************************************************
+
 		// 集合
-		else if (value instanceof Collection){
-			flag = ((Collection) value).isEmpty();
+		if (value instanceof Collection){
+			return ((Collection<?>) value).isEmpty();
 		}
+
 		// map
-		else if (value instanceof Map){
-			flag = ((Map) value).isEmpty();
+		if (value instanceof Map){
+			return ((Map<?, ?>) value).isEmpty();
 		}
+
 		// 枚举
-		else if (value instanceof Enumeration){
-			flag = !((Enumeration) value).hasMoreElements();
+		if (value instanceof Enumeration){
+			return !((Enumeration<?>) value).hasMoreElements();
 		}
+
 		// Iterator迭代器
-		else if (value instanceof Iterator){
-			flag = !((Iterator) value).hasNext();
+		if (value instanceof Iterator){
+			return !((Iterator<?>) value).hasNext();
 		}
-		return flag;
+
+		boolean arrayFlag = arrayIsNullOrEmpty(value);
+		if (arrayFlag){
+			return true;
+		}
+		// 这里可以扩展
+		return false;
 	}
 
 	/**
@@ -114,18 +140,147 @@ public final class Validator{
 	 * <ol>
 	 * <li>{@link Collection},使用其 {@link Collection#isEmpty()}</li>
 	 * <li>{@link Map},使用其 {@link Map#isEmpty()}</li>
-	 * <li><code>Object[]</code>,判断length==0</li>
 	 * <li>{@link String},使用 {@link String#trim()}.length()<=0效率高</li>
 	 * <li>{@link Enumeration},使用 {@link Enumeration#hasMoreElements()}</li>
 	 * <li>{@link Iterator},使用 {@link Iterator#hasNext()}</li>
+	 * <li><code>Object[]</code>,判断length==0</li>
+	 * <li><code>byte[]</code>,判断length==0</li>
+	 * <li><code>boolean[]</code>,判断length==0</li>
+	 * <li><code>char[]</code>,判断length==0</li>
+	 * <li><code>int[]</code>,判断length==0</li>
+	 * <li><code>short[]</code>,判断length==0</li>
+	 * <li><code>float[]</code>,判断length==0</li>
+	 * <li><code>double[]</code>,判断length==0</li>
 	 * </ol>
 	 * 
 	 * @param value
-	 *            可以是Collection,Map,Object[],String,Enumeration,Iterator类型
-	 * @return 如果是null,返回false;如果是空也返回false,其他情况返回true
+	 *            可以是Collection,Map,String,Enumeration,Iterator,以及所有数组类型
+	 * @return 如果是null,返回false<br>
+	 *         如果是空也返回false<br>
+	 *         其他情况返回true<br>
+	 *         如果不是上述类型,不判断empty,返回true
+	 * @see CollectionUtils#isEmpty(Collection)
+	 * @see CollectionUtils#isNotEmpty(Collection)
+	 * @see StringUtils#isBlank(String)
+	 * @see StringUtils#isEmpty(String)
+	 * @see MapUtils#isEmpty(Map)
+	 * @see MapUtils#isNotEmpty(Map)
+	 * @see ArrayUtils#isEmpty(byte[])
+	 * @see ArrayUtils#isEmpty(boolean[])
+	 * @see ArrayUtils#isEmpty(char[])
+	 * @see ArrayUtils#isEmpty(int[])
+	 * @see ArrayUtils#isEmpty(long[])
+	 * @see ArrayUtils#isEmpty(short[])
+	 * @see ArrayUtils#isEmpty(float[])
+	 * @see ArrayUtils#isEmpty(double[])
+	 * @see ArrayUtils#isEmpty(Object[])
 	 */
 	public final static boolean isNotNullOrEmpty(Object value){
 		return !isNullOrEmpty(value);
 	}
 
+	/**
+	 * 数组 类型的验证,区分 primitive 和包装类型.
+	 * 
+	 * @param value
+	 *            可以是
+	 *            <ul>
+	 *            <li>Object[] 二维数组属于这个类型</li>
+	 *            <li>byte[]</li>
+	 *            <li>boolean[]</li>
+	 *            <li>char[]</li>
+	 *            <li>int[]</li>
+	 *            <li>long[]</li>
+	 *            <li>short[]</li>
+	 *            <li>float[]</li>
+	 *            <li>double[]</li>
+	 *            </ul>
+	 * @return 如果是数组类型(区分 primitive和包装类型),判断其length==0;<br>
+	 *         如果不是 直接返回false
+	 * @see ArrayUtils#isEmpty(byte[])
+	 * @see ArrayUtils#isEmpty(boolean[])
+	 * @see ArrayUtils#isEmpty(char[])
+	 * @see ArrayUtils#isEmpty(int[])
+	 * @see ArrayUtils#isEmpty(long[])
+	 * @see ArrayUtils#isEmpty(short[])
+	 * @see ArrayUtils#isEmpty(float[])
+	 * @see ArrayUtils#isEmpty(double[])
+	 * @see ArrayUtils#isEmpty(Object[])
+	 * @since 1.0.7
+	 */
+	private static boolean arrayIsNullOrEmpty(Object value){
+		// ***********************************************************
+		// 数组 Integer/String...自定义的对象User.等数组也 instanceof Object[]
+		if (value instanceof Object[]){
+			// if (log.isDebugEnabled()){
+			// log.debug("value:{} instanceof Object[]", JsonUtil.format(value));
+			// }
+			return ((Object[]) value).length == 0;
+		}
+
+		// primitive ints
+		if (value instanceof int[]){
+			// if (log.isDebugEnabled()){
+			// log.debug("value:{} instanceof int[]", value);
+			// }
+			return ((int[]) value).length == 0;
+		}
+
+		// primitive long
+		if (value instanceof long[]){
+			// if (log.isDebugEnabled()){
+			// log.debug("value:{} instanceof long[]", value);
+			// }
+			return ((long[]) value).length == 0;
+		}
+
+		// primitive float
+		if (value instanceof float[]){
+			// if (log.isDebugEnabled()){
+			// log.debug("value:{} instanceof float[]", value);
+			// }
+			return ((float[]) value).length == 0;
+		}
+
+		// primitive double
+		if (value instanceof double[]){
+			// if (log.isDebugEnabled()){
+			// log.debug("value:{} instanceof double[]", value);
+			// }
+			return ((double[]) value).length == 0;
+		}
+
+		// primitive char
+		if (value instanceof char[]){
+			// if (log.isDebugEnabled()){
+			// log.debug("value:{} instanceof char[]", value);
+			// }
+			return ((char[]) value).length == 0;
+		}
+
+		// primitive boolean
+		if (value instanceof boolean[]){
+			// if (log.isDebugEnabled()){
+			// log.debug("value:{} instanceof boolean[]", value);
+			// }
+			return ((boolean[]) value).length == 0;
+		}
+
+		// primitive byte
+		if (value instanceof byte[]){
+			// if (log.isDebugEnabled()){
+			// log.debug("value:{} instanceof byte[]", value);
+			// }
+			return ((byte[]) value).length == 0;
+		}
+
+		// primitive short
+		if (value instanceof short[]){
+			// if (log.isDebugEnabled()){
+			// log.debug("value:{} instanceof short[]", value);
+			// }
+			return ((short[]) value).length == 0;
+		}
+		return false;
+	}
 }
