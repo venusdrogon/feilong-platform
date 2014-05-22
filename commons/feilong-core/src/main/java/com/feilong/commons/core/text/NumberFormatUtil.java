56e5ce55e29de6f7366e20bda47e805cf87b106c
feilong <venusdrogon@163.com>
2014-05-23 00:42:37 +0700

    add NumberPattern PERCENT_WITH_1POINT
    add NumberFormatUtil.format(Number value,String
    numberPattern,RoundingMode roundingMode)

--- commons/feilong-core/src/main/java/com/feilong/commons/core/text/NumberFormatUtil.java
+++ commons/feilong-core/src/main/java/com/feilong/commons/core/text/NumberFormatUtil.java
@@ -15,6 +15,8 @@
  */
 package com.feilong.commons.core.text;
 
+import java.math.RoundingMode;
+import java.text.ChoiceFormat;
 import java.text.DecimalFormat;
 import java.text.Format;
 import java.text.NumberFormat;
@@ -22,40 +24,97 @@ import java.text.NumberFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.feilong.commons.core.util.NumberPattern;
+
 /**
- * NumberFormat 是所有数值格式的抽象基类,此类提供格式化和解析数值的接口<br>
- * 直接已知子类： ChoiceFormat, DecimalFormat.
+ * {@link NumberFormat}是所有数值格式的抽象基类,此类提供格式化和解析数值的接口
+ * <p>
+ * 直接已知子类： {@link ChoiceFormat}, {@link DecimalFormat}.<br>
+ * 注意:<span style="color:red">{@link DecimalFormat}不是同步的 </span>,建议为每个线程创建独立的格式实例。 (见JAVA API 文档)
+ * </p>
  * 
  * @author <a href="mailto:venusdrogon@163.com">金鑫</a>
  * @version 1.0 2012-3-27 上午1:39:53
  * @see Format
  * @see NumberFormat
  * @see DecimalFormat
+ * @see NumberPattern
  * @since 1.0.0
  */
-public class NumberFormatUtil{
+public final class NumberFormatUtil{
 
 	/** The Constant log. */
 	private static final Logger	log	= LoggerFactory.getLogger(NumberFormatUtil.class);
 
 	/**
-	 * Format.
+	 * 将 {@link Number} 使用 numberPattern格式化.
+	 * <p>
+	 * 该方法使用RoundingMode.HALF_UP
+	 * </p>
 	 * 
 	 * @param value
 	 *            the value
-	 * @param pattern
-	 *            the pattern
-	 * @return the string<br>
-	 *         如果有异常 将返回null
+	 * @param numberPattern
+	 *            the pattern {@link NumberPattern}
+	 * @return 如果有异常 将返回null
+	 * @see NumberPattern
+	 * @see DecimalFormat
+	 * @see RoundingMode#HALF_UP
+	 * @throws NullPointerException
+	 *             if null == value or null == numberPattern
 	 */
-	public static String format(Number value,String pattern){
+	public static String format(Number value,String numberPattern) throws NullPointerException{
+		// 如果不设置, DecimalFormat默认使用的是 RoundingMode.HALF_EVEN
+		RoundingMode roundingMode = RoundingMode.HALF_UP;
+		return format(value, numberPattern, roundingMode);
+	}
+
+	/**
+	 * 将 {@link Number} 使用 {@link RoundingMode} numberPattern格式化.
+	 * 
+	 * @param value
+	 *            the value
+	 * @param numberPattern
+	 *            the pattern {@link NumberPattern}
+	 * @param roundingMode
+	 *            四舍五入的方法{@link RoundingMode} 参考:{@link <a href="../util/NumberUtil.html#RoundingMode">JAVA 8种舍入法</a>}
+	 * @return 如果有异常 将返回null
+	 * @throws NullPointerException
+	 *             if null == value or null == numberPattern
+	 * @see NumberPattern
+	 * @see DecimalFormat
+	 * @see <a href="../util/NumberUtil.html#RoundingMode">JAVA 8种舍入法</a>
+	 */
+	public static String format(Number value,String numberPattern,RoundingMode roundingMode) throws NullPointerException{
+		if (null == value){
+			throw new NullPointerException("the value is null or empty!");
+		}
+
+		if (null == numberPattern){
+			throw new NullPointerException("the numberPattern is null or empty!");
+		}
+
 		try{
-			DecimalFormat decimalFormat = new DecimalFormat(pattern);
+			DecimalFormat decimalFormat = new DecimalFormat(numberPattern);
+
+			if (null != roundingMode){
+				// 默认使用的是 RoundingMode.HALF_EVEN
+				// 精确舍入，银行家舍入法。四舍六入，五分两种情况。如果前一位为奇数，则入位，否则舍去。以下例子为保留小数点1位，那么这种舍入方式下的结果。
+				// 1.15>1.2    1.25>1.2 
+				decimalFormat.setRoundingMode(roundingMode);
+			}
+
 			// decimalFormat.applyPattern("##,###.000");
-			return decimalFormat.format(value);
+			String format = decimalFormat.format(value);
+
+			if (log.isDebugEnabled()){
+				log.debug("value:[{}], pattern:[{}],return:[{}]", value, numberPattern, format);
+			}
+			return format;
 		}catch (Exception e){
-			Object[] objects = { e.getMessage(), value, pattern };
-			log.error("{},传入的参数为,[value:{}],[pattern:{}]", objects);
+			Object[] objects = { e.getMessage(), value, numberPattern };
+			log.error("{},value:[{}],pattern:[{}]", objects);
+			e.printStackTrace();
 		}
 		return null;
 	}
--- commons/feilong-core/src/main/java/com/feilong/commons/core/util/NumberPattern.java
+++ commons/feilong-core/src/main/java/com/feilong/commons/core/util/NumberPattern.java
@@ -22,69 +22,87 @@ import java.text.DecimalFormat;
  * 数字格式,内置常用数字格式
  * </p>
  * <blockquote>
- * <table border=0 cellspacing=3 cellpadding=0 summary="Chart showing symbol, * location, localized, and meaning.">
+ * <table border=1 cellspacing=0 cellpadding=4 summary="Chart showing symbol, * location, localized, and meaning.">
  * <tr bgcolor="#ccccff">
- * <th align=left>Symbol
- * <th align=left>Location
- * <th align=left>Localized?
- * <th align=left>Meaning
+ * <th align=left>Symbol</th>
+ * <th align=left>Location</th>
+ * <th align=left>Localized?</th>
+ * <th align=left>Meaning</th>
+ * </tr>
  * <tr valign=top>
- * <td><code>0</code>
- * <td>Number
- * <td>Yes
- * <td>Digit
+ * <td><code>0</code></td>
+ * <td>Number</td>
+ * <td>Yes</td>
+ * <td>代表阿拉伯数字，每一个0表示一位阿拉伯数字， 如果该位不存在则显示0<br>
+ * 如果对应位置上没有数字，则用零代替</td>
+ * </tr>
  * <tr valign=top bgcolor="#eeeeff">
- * <td><code>#</code>
- * <td>Number
- * <td>Yes
- * <td>Digit, zero shows as absent
+ * <td><code>#</code></td>
+ * <td>Number</td>
+ * <td>Yes</td>
+ * <td>代表阿拉伯数字，每一个#表示一位阿拉伯数字， 如果该位不存在则不显示<br>
+ * 如果对应位置上没有数字，则保持原样（不用补）；如果最前、后为0，则保持为空。</td>
+ * </tr>
  * <tr valign=top>
- * <td><code>.</code>
- * <td>Number
- * <td>Yes
- * <td>Decimal separator or monetary decimal separator
+ * <td><code>.</code></td>
+ * <td>Number</td>
+ * <td>Yes</td>
+ * <td>小数点分隔符或货币的小数分隔符</td>
+ * </tr>
  * <tr valign=top bgcolor="#eeeeff">
- * <td><code>-</code>
- * <td>Number
- * <td>Yes
- * <td>Minus sign
+ * <td><code>-</code></td>
+ * <td>Number</td>
+ * <td>Yes</td>
+ * <td>Minus sign 代表负号</td>
+ * </tr>
  * <tr valign=top>
- * <td><code>,</code>
- * <td>Number
- * <td>Yes
- * <td>Grouping separator
+ * <td><code>,</code></td>
+ * <td>Number</td>
+ * <td>Yes</td>
+ * <td>Grouping separator分组分隔符</td>
+ * </tr>
  * <tr valign=top bgcolor="#eeeeff">
- * <td><code>E</code>
- * <td>Number
- * <td>Yes
- * <td>Separates mantissa and exponent in scientific notation. <em>Need not be quoted in prefix or suffix.</em>
+ * <td><code>E</code></td>
+ * <td>Number</td>
+ * <td>Yes</td>
+ * <td>Separates mantissa and exponent in scientific notation. <em>Need not be quoted in prefix or suffix.</em> 分隔科学计数法中的尾数和指数</td>
+ * </tr>
  * <tr valign=top>
- * <td><code>;</code>
- * <td>Subpattern boundary
- * <td>Yes
- * <td>Separates positive and negative subpatterns
+ * <td><code>;</code></td>
+ * <td>Subpattern boundary</td>
+ * <td>Yes</td>
+ * <td>Separates positive and negative subpatterns</td>
+ * </tr>
  * <tr valign=top bgcolor="#eeeeff">
- * <td><code>%</code>
- * <td>Prefix or suffix
- * <td>Yes
- * <td>Multiply by 100 and show as percentage
+ * <td><code>%</code></td>
+ * <td>Prefix or suffix</td>
+ * <td>Yes</td>
+ * <td>Multiply by 100 and show as percentage数字乘以100并显示为百分数</td>
+ * </tr>
  * <tr valign=top>
- * <td><code>&#92;u2030</code>
- * <td>Prefix or suffix
- * <td>Yes
- * <td>Multiply by 1000 and show as per mille value
+ * <td><code>&#92;u2030</code></td>
+ * <td>Prefix or suffix</td>
+ * <td>Yes</td>
+ * <td>Multiply by 1000 and show as per mille value乘以1000并显示为千分数</td>
+ * </tr>
  * <tr valign=top bgcolor="#eeeeff">
- * <td><code>&#164;</code> (<code>&#92;u00A4</code>)
- * <td>Prefix or suffix
- * <td>No
+ * <td><code>&#164;</code> (<code>&#92;u00A4</code>)</td>
+ * <td>Prefix or suffix</td>
+ * <td>No</td>
  * <td>Currency sign, replaced by currency symbol. If doubled, replaced by international currency symbol. If present in a pattern, the
- * monetary decimal separator is used instead of the decimal separator.
+ * monetary decimal separator is used instead of the decimal separator.<br>
+ * 货币记号，由货币号替换。<br>
+ * 如果两个同时出现，则用国际货币符号替换； 如果出现在某个模式中，则使用货币小数分隔符，而不使用小数分隔符</td>
+ * </tr>
  * <tr valign=top>
- * <td><code>'</code>
- * <td>Prefix or suffix
- * <td>No
+ * <td><code>'</code></td>
+ * <td>Prefix or suffix</td>
+ * <td>No</td>
  * <td>Used to quote special characters in a prefix or suffix, for example, <code>"'#'#"</code> formats 123 to <code>"#123"</code>. To
- * create a single quote itself, use two in a row: <code>"# o''clock"</code>.
+ * create a single quote itself, use two in a row: <code>"# o''clock"</code>. <br>
+ * 用于在前缀或后缀中为特殊字符加引号， 例如 "'#'#" 将 123 格式化为 "#123"。<br>
+ * 要创建单引号本身，则连续使用两个单引号，例如"# o''clock"</td>
+ * </tr>
  * </table>
  * </blockquote>
  * 
@@ -96,9 +114,17 @@ import java.text.DecimalFormat;
  */
 public interface NumberPattern{
 
+	/** 百分数的表达式(不带小数) <code>{@value}</code>. */
+	String	PERCENT_WITH_NOPOINT	= "##%";
+
 	/** 百分数的表达式(2位小数点) <code>{@value}</code>. */
 	String	PERCENT_WITH_2POINT		= "#0.00%";
 
-	/** 百分数的表达式(无小数点) <code>{@value}</code>. */
-	String	PERCENT_WITH_NOPOINT	= "##%";
+	/**
+	 * 百分数的表达式(1位小数点) <code>{@value}</code>.
+	 * 
+	 * @since 1.0.7
+	 */
+	String	PERCENT_WITH_1POINT		= "#0.0%";
+
 }
