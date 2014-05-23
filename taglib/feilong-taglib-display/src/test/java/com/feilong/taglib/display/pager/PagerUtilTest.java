5299d63172dabbad6859cb76d0165abf04277608
feilong <venusdrogon@163.com>
2014-05-23 16:30:46 +0700

    update log

--- taglib/feilong-taglib-display/src/test/java/com/feilong/taglib/display/pager/PagerUtilTest.java
+++ taglib/feilong-taglib-display/src/test/java/com/feilong/taglib/display/pager/PagerUtilTest.java
@@ -1,17 +1,17 @@
-/**
- * Copyright (c) 2008-2014 FeiLong, Inc. All Rights Reserved.
- * <p>
- * 	This software is the confidential and proprietary information of FeiLong Network Technology, Inc. ("Confidential Information").  <br>
- * 	You shall not disclose such Confidential Information and shall use it 
- *  only in accordance with the terms of the license agreement you entered into with FeiLong.
- * </p>
- * <p>
- * 	FeiLong MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, 
- * 	INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * 	PURPOSE, OR NON-INFRINGEMENT. <br> 
- * 	FeiLong SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
- * 	THIS SOFTWARE OR ITS DERIVATIVES.
- * </p>
+/*
+ * Copyright (C) 2008 feilong (venusdrogon@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.feilong.taglib.display.pager;
 
@@ -34,23 +34,34 @@ import com.feilong.taglib.display.pager.command.PagerParams;
 
 /**
  * The Class PagerUtilTest.
+ * 
+ * @author <a href="mailto:venusdrogon@163.com">feilong</a>
+ * @version 1.0.7 2014-5-23 15:58:19
  */
 public class PagerUtilTest{
 
 	/** The Constant log. */
 	private static final Logger	log					= LoggerFactory.getLogger(PagerUtilTest.class);
 
+	/** The en loc. */
 	private Locale				enLoc				= new Locale("en", "US");						// 表示美国地区
 
+	/** The fr loc. */
 	private Locale				frLoc				= new Locale("fr", "FR");						// 表示法国地区
 
+	/** The zh loc. */
 	private Locale				zhLoc				= new Locale("zh", "CN");						// 表示中国地区
 
 	// Locale.ENGLISH;
+	/** The locale. */
 	private Locale				locale				= Locale.SIMPLIFIED_CHINESE;
 
+	/** The debug is not parse vm. */
 	boolean						debugIsNotParseVM	= false;
 
+	/**
+	 * Name.
+	 */
 	@Test
 	public void name(){
 		if (log.isInfoEnabled()){
@@ -63,6 +74,13 @@ public class PagerUtilTest{
 		}
 	}
 
+	/**
+	 * Gets the pager content.
+	 * 
+	 * @return the pager content
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
 	@SuppressWarnings({ "javadoc", "unused" })
 	@Test
 	public void getPagerContent() throws IOException{
@@ -112,7 +130,9 @@ public class PagerUtilTest{
 	 * Test method for.
 	 * {@link com.feilong.taglib.display.pager.PagerUtil#getPagerContent(int, int, int, int, java.lang.String, java.lang.String, java.lang.String, java.lang.String)}
 	 * .
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
 	 */
 	@Test
 	public void testGetPagerContent() throws IOException{
