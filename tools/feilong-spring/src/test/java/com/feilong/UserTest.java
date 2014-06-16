5670d2aac7a5866dea85da4e73550e735834058c
feilong <venusdrogon@163.com>
2014-06-16 10:33:05 +0800

    test spring array inject

--- tools/feilong-spring/src/test/java/com/feilong/UserTest.java
+++ tools/feilong-spring/src/test/java/com/feilong/UserTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2008 feilong (venusdrogon@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.feilong;
 
 import java.util.HashSet;
@@ -8,6 +23,8 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.Vector;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,33 +36,44 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 import com.feilong.commons.core.tools.json.JsonUtil;
 import com.feilong.entity.User;
 
-
+/**
+ * The Class UserTest.
+ * 
+ * @author <a href="mailto:venusdrogon@163.com">feilong</a>
+ * @version 1.0.7 2014-6-16 1:09:06
+ */
 public class UserTest implements ApplicationContextAware{
 
-	private static final Logger			log		= LoggerFactory.getLogger(UserTest.class);
+	/** The Constant log. */
+	private static final Logger			log			= LoggerFactory.getLogger(UserTest.class);
 
-	private static ApplicationContext	context	= null;
+	/** The context. */
+	private static ApplicationContext	context1	= null;
+
+	private static ApplicationContext	fileSystemContext;
 
 	// @Value("#{testProperties['name']}")
 	// private String aString;
-	@Test
-	public void test(){
-		ApplicationContext context = new FileSystemXmlApplicationContext("classpath:spring.xml");
-		Properties props = context.getBean("testProperties", Properties.class);
-		// log.info(aString);
+	@BeforeClass
+	public static void beforeClass(){
+		fileSystemContext = new FileSystemXmlApplicationContext("classpath:spring.xml");
+	}
 
-		log.info("props:{}", JsonUtil.format(props));
-		// Locale locale = new Locale("zh", "CN");
-		// log.info("================" + context.getMessage("name", null, locale));
-		User user = (User) context.getBean("feitian@");
+	/**
+	 * Test.
+	 */
+	@Test
+	public void testUser(){
+		User user = (User) fileSystemContext.getBean("feitian@");
+		if (log.isInfoEnabled()){
+			log.info(JsonUtil.format(user));
+		}
 		log.info(user.getUserName());
 		// -----------------------------
 		List<String> list = user.getList();
 		log.info("list:{}", JsonUtil.format(list));
 		// --------------------------------
 		Map<String, Object> map = user.getMap();
-		map.put(null, null);
-		map.put(null, 222);
 		log.info("map:{}", JsonUtil.format(map));
 		List<String> list2 = (List<String>) map.get("五子良将");
 		log.info("list2:{}", JsonUtil.format(list2));
@@ -60,7 +88,10 @@ public class UserTest implements ApplicationContextAware{
 		set.add("1");
 		set.add("1");
 		log.info("set:{}", JsonUtil.format(set));
-		// *******************************
+	}
+
+	@Test
+	public void testVector(){
 		Vector<Integer> vector = new Vector<Integer>();
 		vector.add(1);
 		vector.add(2222);
@@ -68,13 +99,32 @@ public class UserTest implements ApplicationContextAware{
 		vector.add(55555);
 		log.info("vector:{}", JsonUtil.format(vector));
 		log.info("" + vector.get(0));
-		// /********************************************
+	}
+
+	@Test
+	public void testHashtable(){
 		Hashtable<String, Object> hashtable = new Hashtable<String, Object>();
 		hashtable.put("a", "a");
 		// hashtable.put("a", null);
 		log.info("hashtable:{}", JsonUtil.format(hashtable));
 	}
 
+	/**
+	* 
+	*/
+	@Test
+	public void testUtilProperties(){
+		Properties props = fileSystemContext.getBean("testProperties", Properties.class);
+		// log.info(aString);
+		log.info("props:{}", JsonUtil.format(props));
+
+		// Locale locale = new Locale("zh", "CN");
+		// log.info("================" + context.getMessage("name", null, locale));
+	}
+
+	/**
+	 * Hash set.
+	 */
 	@Test
 	public void hashSet(){
 		// **********************************************************
@@ -89,14 +139,22 @@ public class UserTest implements ApplicationContextAware{
 		log.info("hashSet1:{}", JsonUtil.format(hashSet1));
 	}
 
+	/**
+	 * Test1.
+	 */
 	@Test
 	public void test1(){
-		User user = (User) context.getBean("feitian@");
+		User user = (User) context1.getBean("feitian@");
 		log.info("user:{}", JsonUtil.format(user));
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.ApplicationContext)
+	 */
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException{
-		context = applicationContext;
+		context1 = applicationContext;
 	}
 }
--- tools/feilong-spring/src/test/java/com/feilong/entity/User.java
+++ tools/feilong-spring/src/test/java/com/feilong/entity/User.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2008 feilong (venusdrogon@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.feilong.entity;
 
 import java.util.List;
@@ -11,83 +26,197 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+/**
+ * The Class User.
+ * 
+ * @author <a href="mailto:venusdrogon@163.com">feilong</a>
+ * @version 1.0.7 2014-6-16 0:42:08
+ */
 public class User implements InitializingBean,DisposableBean{
 
+	/** The Constant log. */
 	private final static Logger	log	= LoggerFactory.getLogger(User.class);
 
+	/** The user name. */
 	private String				userName;
 
+	/** The array. */
+	private String[]			array;
+
+	/** The list. */
 	private List<String>		list;
 
+	/** The map. */
 	private Map<String, Object>	map;
 
+	/** The properties. */
 	private Properties			properties;
 
+	/** The set. */
 	private Set<String>			set;
 
+	/** The vector. */
 	private Vector<Object>		vector;
 
-	public String getUserName(){
-		return userName;
-	}
-
-	public void setUserName(String userName){
-		this.userName = userName;
-	}
-
+	/**
+	 * Inits the.
+	 */
 	public void init(){
 		log.info("init.....");
 		this.userName = "feilong";
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+	 */
 	@Override
 	public void afterPropertiesSet() throws Exception{
 		log.info("afterPropertiesSet.....");
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.beans.factory.DisposableBean#destroy()
+	 */
 	@Override
 	public void destroy() throws Exception{
 		log.info("destroy.....");
 		this.userName = null;
 	}
 
+	/**
+	 * Gets the list.
+	 * 
+	 * @return the list
+	 */
 	public List<String> getList(){
 		return list;
 	}
 
+	/**
+	 * Sets the list.
+	 * 
+	 * @param list
+	 *            the new list
+	 */
 	public void setList(List<String> list){
 		this.list = list;
 	}
 
+	/**
+	 * Gets the map.
+	 * 
+	 * @return the map
+	 */
 	public Map<String, Object> getMap(){
 		return map;
 	}
 
+	/**
+	 * 设置 map.
+	 * 
+	 * @param map
+	 *            the map
+	 */
 	public void setMap(Map<String, Object> map){
 		this.map = map;
 	}
 
+	/**
+	 * Gets the properties.
+	 * 
+	 * @return the properties
+	 */
 	public Properties getProperties(){
 		return properties;
 	}
 
+	/**
+	 * Sets the properties.
+	 * 
+	 * @param properties
+	 *            the new properties
+	 */
 	public void setProperties(Properties properties){
 		this.properties = properties;
 	}
 
+	/**
+	 * Gets the 设置.
+	 * 
+	 * @return the 设置
+	 */
 	public Set<String> getSet(){
 		return set;
 	}
 
+	/**
+	 * Sets the 设置.
+	 * 
+	 * @param set
+	 *            the new 设置
+	 */
 	public void setSet(Set<String> set){
 		this.set = set;
 	}
 
+	/**
+	 * Gets the vector.
+	 * 
+	 * @return the vector
+	 */
 	public Vector<Object> getVector(){
 		return vector;
 	}
 
+	/**
+	 * Sets the vector.
+	 * 
+	 * @param vector
+	 *            the new vector
+	 */
 	public void setVector(Vector<Object> vector){
 		this.vector = vector;
+	}
+
+	/**
+	 * Gets the array.
+	 * 
+	 * @return the array
+	 */
+	public String[] getArray(){
+		return array;
+	}
+
+	/**
+	 * Sets the array.
+	 * 
+	 * @param array
+	 *            the array to set
+	 */
+	public void setArray(String[] array){
+		this.array = array;
+	}
+
+	/**
+	 * Gets the user name.
+	 * 
+	 * @return the user name
+	 */
+	public String getUserName(){
+		return userName;
+	}
+
+	/**
+	 * Sets the user name.
+	 * 
+	 * @param userName
+	 *            the new user name
+	 */
+	public void setUserName(String userName){
+		this.userName = userName;
 	}
 }
