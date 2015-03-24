2427c2885497011b2e9efb91bff758add6ec0b25
feilong <venusdrogon@163.com>
2015-03-24 15:07:46 +0800

    update javadoc
    pom jdk change to 1.7

--- commons/feilong-servlet/src/main/java/com/feilong/servlet/http/SessionUtil.java
+++ commons/feilong-servlet/src/main/java/com/feilong/servlet/http/SessionUtil.java
@@ -51,7 +51,7 @@ public final class SessionUtil{
 
     /** Don't let anyone instantiate this class. */
     private SessionUtil(){
-        //AssertionError不是必须的。但它可以避免不小心在类的内部调用构造器。保证该类在任何情况下都不会被实例化。
+        //AssertionError不是必须的. 但它可以避免不小心在类的内部调用构造器. 保证该类在任何情况下都不会被实例化.
         //see 《Effective Java》 2nd
         throw new AssertionError("No " + getClass().getName() + " instances for you!");
     }
