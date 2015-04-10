77ce54071e1f914fe8618fa1e94ffaca9b1a42d1
feilong <venusdrogon@163.com>
2015-04-10 15:59:38 +0800

    add ThreadLogSwitch

--- commons/feilong-core/src/main/java/com/feilong/commons/core/lang/ThreadUtil.java
+++ commons/feilong-core/src/main/java/com/feilong/commons/core/lang/ThreadUtil.java
@@ -15,27 +15,36 @@
  */
 package com.feilong.commons.core.lang;
 
-import java.util.HashMap;
+import java.lang.Thread.State;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.feilong.commons.core.lang.entity.ThreadLogSwitch;
+import com.feilong.commons.core.log.Slf4jUtil;
 import com.feilong.commons.core.tools.json.JsonUtil;
+import com.feilong.commons.core.util.Validator;
 
 /**
  * 线程解析.
- * 
+ *
  * @author <a href="mailto:venusdrogon@163.com">金鑫</a>
- * @version 1.0 Dec 17, 2011 2:00:35 AM
+ * @version 1.0.0 Dec 17, 2011 2:00:35 AM
+ * @see java.lang.Thread
+ * @see java.lang.Thread#currentThread()
  * @since 1.0.0
- * @deprecated 不建议使用
  */
-@Deprecated
 public final class ThreadUtil{
 
     /** The Constant log. */
-    private static final Logger log = LoggerFactory.getLogger(ThreadUtil.class);
+    private static final Logger          log           = LoggerFactory.getLogger(ThreadUtil.class);
+
+    /** 简单开关,只显示id name 和活跃线程数量 . */
+    private static final ThreadLogSwitch SIMPLE_SWITCH = new ThreadLogSwitch(true, true, true);
 
     /** Don't let anyone instantiate this class. */
     private ThreadUtil(){
@@ -55,13 +64,12 @@ public final class ThreadUtil{
         // 允许线程访问有关自己的线程组的信息，但是不允许它访问有关其线程组的父线程组或其他任何线程组的信息.
         Thread currentThread = Thread.currentThread();
 
-        Map<String, Object> threadObjectLog = getThreadObjectLog(currentThread);
-        log.debug(JsonUtil.format(threadObjectLog));
+        log.debug(JsonUtil.format(getCurrentThreadMapForLog()));
 
         ThreadGroup currentThreadGroup = currentThread.getThreadGroup();
         ThreadGroup topThreadGroup = getTopThreadGroup(currentThreadGroup);
 
-        log.debug(JsonUtil.format(getThreadGroupObjectLog(topThreadGroup)));
+        log.debug(JsonUtil.format(getThreadGroupInfoMapForLog(topThreadGroup)));
         // 返回此线程组中活动线程的估计数.
         int topThreadGroupActiveCount = topThreadGroup.activeCount();
 
@@ -76,8 +84,7 @@ public final class ThreadUtil{
      * @return the top thread group
      */
     private static ThreadGroup getTopThreadGroup(ThreadGroup threadGroup){
-
-        log.debug(JsonUtil.format(getThreadGroupObjectLog(threadGroup)));
+        log.debug(JsonUtil.format(getThreadGroupInfoMapForLog(threadGroup)));
 
         ThreadGroup parentThreadGroup = threadGroup.getParent();
         if (parentThreadGroup == null){
@@ -86,12 +93,6 @@ public final class ThreadUtil{
             // 递归写法
             return getTopThreadGroup(parentThreadGroup);
         }
-
-        // 循环写法
-        // for (; threadGroup.getParent() != null; threadGroup = threadGroup.getParent()){
-
-        // }
-        // return threadGroup;
     }
 
     /**
@@ -101,10 +102,10 @@ public final class ThreadUtil{
      *            the thread group
      * @return the thread group object log
      */
-    public static Map<String, Object> getThreadGroupObjectLog(ThreadGroup threadGroup){
+    public static Map<String, Object> getThreadGroupInfoMapForLog(ThreadGroup threadGroup){
 
         if (null != threadGroup){
-            Map<String, Object> map = new HashMap<String, Object>();
+            Map<String, Object> map = new LinkedHashMap<String, Object>();
 
             // 返回此线程组中活动线程的估计数.
             map.put("threadGroup.activeCount()", threadGroup.activeCount());
@@ -135,38 +136,147 @@ public final class ThreadUtil{
      *            当某个线程中运行的代码创建一个新 Thread 对象时，该新线程的初始优先级被设定为创建线程的优先级，并且当且仅当创建线程是守护线程时，新线程才是守护程序.
      * @return the thread object log
      */
-    public static Map<String, Object> getThreadObjectLog(Thread thread){
+    public static Map<String, Object> getThreadInfoMapForLog(Thread thread){
+        return getThreadInfoMapForLog(thread, SIMPLE_SWITCH);
+    }
+
+    /**
+     * 获得 thread 对象log.
+     *
+     * @param thread
+     *            线程 是程序中的执行线程.Java 虚拟机允许应用程序并发地运行多个执行线程.<br>
+     *            每个线程都有一个优先级，高优先级线程的执行优先于低优先级线程.<br>
+     *            每个线程都可以或不可以标记为一个守护程序.<br>
+     *            当某个线程中运行的代码创建一个新 Thread 对象时，该新线程的初始优先级被设定为创建线程的优先级，并且当且仅当创建线程是守护线程时，新线程才是守护程序.
+     * @param threadLogSwitch
+     *            the thread log config
+     * @return the thread object log
+     * @since 1.1.1
+     */
+    public static Map<String, Object> getThreadInfoMapForLog(Thread thread,ThreadLogSwitch threadLogSwitch){
 
         if (null != thread){
-            Map<String, Object> map = new HashMap<String, Object>();
 
-            // 返回当前线程的线程组中活动线程的数目
-            map.put("Thread.activeCount()", Thread.activeCount());
+            if (Validator.isNullOrEmpty(threadLogSwitch)){
+                threadLogSwitch = SIMPLE_SWITCH;
+            }
 
-            map.put("thread.getContextClassLoader()", thread.getContextClassLoader());
-            map.put("thread.isAlive()", thread.isAlive());
-            map.put("thread.isDaemon()", thread.isDaemon());
-            map.put("thread.isInterrupted()", thread.isInterrupted());
-            // map.put("thread.toString()", thread.toString());
-            map.put("Thread.MIN_PRIORITY", Thread.MIN_PRIORITY);
-            map.put("Thread.MAX_PRIORITY", Thread.MAX_PRIORITY);
-            map.put("Thread.NORM_PRIORITY", Thread.NORM_PRIORITY);
+            Map<String, Object> map = new LinkedHashMap<String, Object>();
 
-            map.put("thread.getId()", thread.getId());
-            map.put("thread.getName()", thread.getName());
+            if (threadLogSwitch.getIsShowId()){
+                //返回该线程的标识符
+                map.put("thread.getId()", thread.getId());
+            }
 
-            map.put("thread.getPriority()", thread.getPriority());
-            StackTraceElement[] stackTraceElement = thread.getStackTrace();
-            map.put("thread.getStackTrace()", stackTraceElement);
-            map.put("thread.getState()", thread.getState());
-            map.put("thread.getThreadGroup()", thread.getThreadGroup());
-            map.put("thread.getUncaughtExceptionHandler()", thread.getUncaughtExceptionHandler());
-            // map.put("Thread.getAllStackTraces()", Thread.getAllStackTraces());
-            map.put("Thread.getDefaultUncaughtExceptionHandler()", Thread.getDefaultUncaughtExceptionHandler());
+            if (threadLogSwitch.getIsShowName()){
+                //返回该线程的名称
+                map.put("thread.getName()", thread.getName());
+            }
+
+            if (threadLogSwitch.getIsShowActiveCount()){
+                // 返回当前线程的线程组中活动线程的数目
+                map.put("Thread.activeCount()", Thread.activeCount());
+            }
+
+            if (threadLogSwitch.getIsShowIsAlive()){
+                //测试线程是否处于活动状态
+                map.put("thread.isAlive()", thread.isAlive());
+            }
+
+            if (threadLogSwitch.getIsShowIsDaemon()){
+                //测试该线程是否为守护线程
+                map.put("thread.isDaemon()", thread.isDaemon());
+            }
+
+            if (threadLogSwitch.getIsShowIsInterrupted()){
+                //测试线程是否已经中断
+                map.put("thread.isInterrupted()", thread.isInterrupted());
+            }
+
+            if (threadLogSwitch.getIsShowMIN_PRIORITY()){
+                map.put("Thread.MIN_PRIORITY", Thread.MIN_PRIORITY);
+            }
+
+            if (threadLogSwitch.getIsShowNORM_PRIORITY()){
+                map.put("Thread.NORM_PRIORITY", Thread.NORM_PRIORITY);
+            }
+
+            if (threadLogSwitch.getIsShowMAX_PRIORITY()){
+                map.put("Thread.MAX_PRIORITY", Thread.MAX_PRIORITY);
+            }
+
+            if (threadLogSwitch.getIsShowPriority()){
+
+                //返回线程的优先级
+                map.put("thread.getPriority()", thread.getPriority());
+            }
+
+            if (threadLogSwitch.getIsShowState()){
+                //返回该线程的状态
+                State state = thread.getState();
+                map.put("thread.getState()", state);
+            }
+
+            if (threadLogSwitch.getIsShowStackTrace()){
+                StackTraceElement[] stackTraceElement = thread.getStackTrace();
+                map.put("thread.getStackTrace()", stackTraceElement);
+            }
+
+            if (threadLogSwitch.getIsShowThreadGroup()){
+                //返回该线程所属的线程组
+                map.put("thread.getThreadGroup()", thread.getThreadGroup());
+            }
+
+            if (threadLogSwitch.getIsShowAllStackTraces()){
+                map.put("Thread.getAllStackTraces()", Thread.getAllStackTraces());
+            }
+
+            if (threadLogSwitch.getIsShowToString()){
+                map.put("thread.toString()", thread.toString());
+            }
+
+            if (threadLogSwitch.getIsShowContextClassLoader()){
+                map.put("thread.getContextClassLoader()", thread.getContextClassLoader());
+            }
+
+            if (threadLogSwitch.getIsShowUncaughtExceptionHandler()){
+                //返回该线程由于未捕获到异常而突然终止时调用的处理程序
+                map.put("thread.getUncaughtExceptionHandler()", thread.getUncaughtExceptionHandler());
+
+            }
+            if (threadLogSwitch.getIsShowDefaultUncaughtExceptionHandler()){
+                map.put("Thread.getDefaultUncaughtExceptionHandler()", Thread.getDefaultUncaughtExceptionHandler());
+            }
 
             return map;
         }
         return null;
+    }
+
+    /**
+     * 获得 thread 对象log.
+     *
+     * @return the thread object log
+     * @since 1.1.1
+     */
+    public static Map<String, Object> getCurrentThreadMapForLog(){
+        return getCurrentThreadMapForLog(SIMPLE_SWITCH);
+    }
+
+    /**
+     * 获得 thread 对象log.
+     *
+     * @param threadLogConfig
+     *            the thread log config
+     * @return the thread object log
+     * @since 1.1.1
+     */
+    public static Map<String, Object> getCurrentThreadMapForLog(ThreadLogSwitch threadLogConfig){
+        // 线程组表示一个线程的集合.此外，线程组也可以包含其他线程组.
+        //线程组构成一棵树，在树中，除了初始线程组外，每个线程组都有一个父线程组.
+        // 允许线程访问有关自己的线程组的信息，但是不允许它访问有关其线程组的父线程组或其他任何线程组的信息.
+        Thread currentThread = Thread.currentThread();
+        return getThreadInfoMapForLog(currentThread, threadLogConfig);
     }
 
     /**
@@ -175,7 +285,9 @@ public final class ThreadUtil{
      * @param currentThread
      *            Thread thread = Thread.currentThread();
      * @return the current method name
+     * @deprecated 不建议使用
      */
+    @Deprecated
     public static String getCurrentMethodName(Thread currentThread){
         return getMethodName(currentThread, 3);
     }
@@ -186,7 +298,9 @@ public final class ThreadUtil{
      * @param currentThread
      *            Thread thread = Thread.currentThread();
      * @return the caller method name
+     * @deprecated 不建议使用
      */
+    @Deprecated
     public static String getCallerMethodName(Thread currentThread){
         return getMethodName(currentThread, 1);
     }
@@ -199,14 +313,34 @@ public final class ThreadUtil{
      * @param index
      *            index 索引
      * @return the method name
+     * @deprecated 不建议使用
      */
+    @Deprecated
     private static String getMethodName(Thread currentThread,int index){
         StackTraceElement[] stackTraceElements = currentThread.getStackTrace();
 
         if (log.isDebugEnabled()){
+
+            List<String> list = new ArrayList<String>();
+
             for (StackTraceElement stackTraceElement : stackTraceElements){
-                log.debug("methodName:{}", stackTraceElement.getMethodName());
+
+                //                String messagePattern = "(%F:%L) [%M()]";
+                String messagePattern = "({}:{}) [{}()]";
+                //                map.put("", stackTraceElement.getClassName()); //com.feilong.commons.core.lang.ThreadUtil
+                //                map.put("", stackTraceElement.isNativeMethod());
+                String fileName = stackTraceElement.getFileName();
+                list.add(Slf4jUtil.formatMessage(
+                                messagePattern,
+                                fileName,
+                                stackTraceElement.getLineNumber(),
+                                stackTraceElement.getMethodName()));
             }
+
+            if (log.isDebugEnabled()){
+                log.debug(JsonUtil.format(list));
+            }
+
         }
         StackTraceElement stackTraceElement = stackTraceElements[index];
         String methodName = stackTraceElement.getMethodName();
