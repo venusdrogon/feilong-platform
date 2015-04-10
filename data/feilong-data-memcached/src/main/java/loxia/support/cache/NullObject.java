e5a9f213601641439e8d7d3f0c759910b97ab0a1
feilong <venusdrogon@163.com>
2015-04-10 18:30:14 +0800

    remove feilong-db-jdbc, feilong-data-solrj4, feilong-data-redis, feilong-data-mongo, feilong-data-memcached

--- data/feilong-data-memcached/src/main/java/loxia/support/cache/NullObject.java
+++
@@ -1,9 +0,0 @@
-package loxia.support.cache;
-
-import java.io.Serializable;
-
-@SuppressWarnings("all")
-public class NullObject implements Serializable{
-
-    private static final long serialVersionUID = 1L;
-}
