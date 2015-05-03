7ef6269f2b88aa7e2f9ea54121d06199c758dca3
feilong <venusdrogon@163.com>
2015-05-03 21:05:16 +0800

    move com.feilong.spring.web.view package to com.feilong.spring.web.servlet

--- spring/feilong-spring-web/src/main/java/com/feilong/spring/web/servlet/view/CommonTiles2View.java
+++ spring/feilong-spring-web/src/main/java/com/feilong/spring/web/servlet/view/CommonTiles2View.java
@@ -1,3 +1,4 @@
+package com.feilong.spring.web.servlet.view;
 ///**
 // * Copyright (c) 2008-2014 FeiLong, Inc. All Rights Reserved.
 // * <p>
--- spring/feilong-spring-web/src/main/java/com/feilong/spring/web/servlet/view/JsonView.java
+++ spring/feilong-spring-web/src/main/java/com/feilong/spring/web/servlet/view/JsonView.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.feilong.spring.web.view;
+package com.feilong.spring.web.servlet.view;
 
 import java.util.Map;
 
