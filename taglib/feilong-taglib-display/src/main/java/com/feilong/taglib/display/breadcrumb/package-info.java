8d8edbad490598ee2cea51db915abf305c351e1b
feilong <venusdrogon@163.com>
2015-05-24 23:29:24 +0800

    remove module taglib

--- taglib/feilong-taglib-display/src/main/java/com/feilong/taglib/display/breadcrumb/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * (面包屑)package feilong,By Jinxin development and maintenance<br>
- * Some commonly used utils <br>
- * 从feilong-commons项目抽取了一小部分,用于brand开发<br>
- * 有兴趣的同事同学可以访问github :<a href="http://venusdrogon.github.io/feilong-platform/">feilong-platform</a>
- */
-package com.feilong.taglib.display.breadcrumb;
